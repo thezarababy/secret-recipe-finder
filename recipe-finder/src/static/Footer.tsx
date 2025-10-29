7382c0fe232381fdf8a8f0cd6692c1cc4d9c18df
thezarababy <jyokoduwa@gmail.com>
2025-10-29 05:43:39 +0100

    footer-responsiveness

--- recipe-finder/src/static/Footer.tsx
+++ recipe-finder/src/static/Footer.tsx
@@ -98,43 +98,31 @@ const Footer = () => {
       <div className="max-w-[1280px] mx-auto flex flex-col md:flex-row justify-between items-center gap-10">
         {/* Logo */}
         <div className="flex items-center gap-4">
-          <h1 className="text-3xl bg-white px-4 py-2 rounded-lg font-bold">
+          <h1 className="text-3xl bg-white px-4 py-2 rounded-lg font-bold max-sm:text-lg max-sm:font-semibold sm:px-1.5">
             Recipe Finder
           </h1>
-          <FcSearch size={40} />
+          <FcSearch size={40} className="sm: size-8" />
         </div>
 
         {/* Links */}
-        <div className="flex flex-col md:flex-row gap-6 text-center md:text-left">
-          <Link
-            to="/"
-            className="text-white text-lg font-medium hover:underline"
-          >
+        <div className="flex flex-row  gap-3 text-center  hover:cursor-pointer max-sm:gap-2  ">
+          <Link to="/" className="text-white text-lg  hover:underline">
             Home
           </Link>
-          <Link
-            to="/category"
-            className="text-white text-lg font-medium hover:underline"
-          >
+          <Link to="/category" className="text-white text-lg  hover:underline">
             Category
           </Link>
-          <Link
-            to="/favorite"
-            className="text-white text-lg font-medium hover:underline"
-          >
+          <Link to="/favorite" className="text-white text-lg  hover:underline">
             Favorite
           </Link>
-          <Link
-            to="/signUp"
-            className="text-white text-lg font-medium hover:underline"
-          >
+          <Link to="/signUp" className="text-white text-lg  hover:underline">
             Sign Up
           </Link>
         </div>
 
         {/* Socials */}
-        <div className="text-center">
-          <h1 className="text-lg font-bold text-white mb-3">Follow us</h1>
+        <div className="text-center flex">
+          <h1 className="text-lg font-bold text-white mb-3 mr-2">Follow us </h1>
           <div className="flex justify-center md:justify-start gap-4">
             <FaFacebookSquare
               size={30}
