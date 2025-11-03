f281196b4c11cf4c343049895bf947f47895824d
thezarababy <jyokoduwa@gmail.com>
2025-11-03 23:08:41 +0100

    login page done

--- recipe-finder/src/component/reuseable/Button.tsx
+++ recipe-finder/src/component/reuseable/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 interface ButtonProps {
   title: string;
@@ -8,25 +8,28 @@ interface ButtonProps {
   iconTitle?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({title, textColor, bgColor, borderColor, iconTitle}) => {
+const Button: React.FC<ButtonProps> = ({
+  title,
+  textColor,
+  bgColor,
+  borderColor,
+  iconTitle,
+}) => {
   return (
     <div>
-        <button className='px-5 py-3 rounded-xl w-max hover:text-xl hover: cursor-pointer'
+      <button
+        className="px-5 py-3 rounded-xl w-full hover:text-xl hover: cursor-pointer"
         style={{
-                color: `${textColor}`,
-                backgroundColor: `${bgColor}`,
-                border: `${borderColor}`
-        }}>
-            <span className='font-medium'>{title}</span>
-            <span>{iconTitle}</span>
-            
-
-
-        </button>
-
-
+          color: `${textColor}`,
+          backgroundColor: `${bgColor}`,
+          border: `${borderColor}`,
+        }}
+      >
+        <span className="font-medium">{title}</span>
+        <span>{iconTitle}</span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- recipe-finder/src/pages/Login.tsx
+++ recipe-finder/src/pages/Login.tsx
@@ -2,8 +2,8 @@ import Logo from "../assets/Logo.png";
 import Button from "../component/reuseable/Button";
 const Login = () => {
   return (
-    <div className="w-full pb-10 ">
-      <nav className="w-full">
+    <div className="w-full  pb-10 ">
+      <nav className="w-full ">
         <img
           src={Logo}
           alt="Logo"
@@ -11,29 +11,29 @@ const Login = () => {
         />
         <hr className=" mx-6 my-3  border-t border-gray-300 " />
       </nav>
-      <h1 className="font-bold text-4xl text-center mt-8 max-sm:text-2xl max-sm:font-semibold ">
+      <h1 className="font-bold text-4xl text-center my-8 max-sm:text-2xl max-sm:font-semibold ">
         Welcome back
       </h1>
-      <section className="w-[500px] m-auto  max-sm:ml-7">
+      <section className=" w-full max-w-[500px]  px-7 flex flex-col gap-7">
         <div>
           <input
             type="text"
             placeholder="Email"
-            className="  pl-2  py-3 w-[500px]  rounded-lg border-0 my-7 bg-gray-300 max-sm:w-auto max-sm:text-sm "
+            className="  pl-2  py-3  w-full max-w-[500px]  rounded-lg border-0 bg-gray-300  max-sm:text-sm "
           />
         </div>
         <div>
           <input
             type="text"
             placeholder="Password"
-            className="pl-2  py-3 w-[500px]  rounded-lg border-0  bg-gray-300 max-sm:w-auto max-sm:text-sm"
+            className="pl-2  py-3  w-full max-w-[500px]  rounded-lg border-0  bg-gray-300 max-sm:text-sm"
           />
         </div>
 
-        <a href="" className="underline inline-block my-4 max-sm:text-sm">
+        <Button title="Log In" bgColor="#FE7B23" textColor="white" />
+        <a href="" className="underline inline-block max-sm:text-sm">
           Forgot password?
         </a>
-        <Button title="Log In" bgColor="#FE7B23" textColor="white" />
       </section>
     </div>
   );
