5599203fd79c794a5be3ae37a60dc3f38e16334e
thezarababy <jyokoduwa@gmail.com>
2025-11-11 11:02:22 +0100

    siggup page 80 percent done

--- recipe-finder/src/pages/SignUp.tsx
+++ recipe-finder/src/pages/SignUp.tsx
@@ -11,29 +11,42 @@ const SignUp = () => {
       </section>
       <section className="flex flex-col items-center py-40 gap-8">
         <h1 className="text-5xl font-bold">Join Our Food Community</h1>
-        <i className="my-0">Find your next favorite recipe</i>
-        <main>
-          <div>
-            <label htmlFor="fullName">Full Name</label>
+        <i className="-mt-4">Find your next favorite recipe!</i>
+        <form action="" className=" flex flex-col gap-5">
+          <div className="">
+            <label htmlFor="fullName">Full Name : </label>
             <input
               type="text"
               id="fullName"
               placeholder="Enter your full name"
+              className="rounded-lg border-0 bg-gray-300 ml-2 px-3 py-2"
             />
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" placeholder="Enter your email" />
+            <label htmlFor="email">Email :</label>
+            <input
+              type="text"
+              id="email"
+              placeholder="Enter your email"
+              className="rounded-lg border-0 bg-gray-300 ml-2 px-3 py-2"
+            />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password :</label>
             <input
               type="text"
               id="password"
               placeholder="Enter your full name"
+              className="rounded-lg border-0 bg-gray-300 ml-2 px-3 py-2"
             />
           </div>
-        </main>
+          <button
+            type="submit"
+            className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded-md transition-colors duration-300"
+          >
+            Create Account
+          </button>
+        </form>
       </section>
     </div>
   );
