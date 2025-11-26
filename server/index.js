6821d8c26a7a056ee3e042ef0eb44eadd1d3e827
thezarababy <jyokoduwa@gmail.com>
2025-11-26 14:09:15 +0100

    index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const connectToDB = require("./config/db.js");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user.route.js");
+app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 connectToDB();
+
+app.use("/api/users", userRouter);
+app.use("/api/product", productRouter);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server up and running ",
+  });
+});
 
 const app = express();
 const PORT = process.env.PORT || 8800;
